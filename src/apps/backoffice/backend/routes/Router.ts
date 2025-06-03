48f54f1a959878e524539d88f8b67d21124c8ce6
AnderGI <agibarguen@gmail.com>
2025-06-03 16:57:02 +0200

    refactor(router): remove container dependency and inject route handlers externally

--- src/apps/backoffice/backend/routes/Router.ts
+++ src/apps/backoffice/backend/routes/Router.ts
@@ -1,16 +1,8 @@
 import type { IncomingMessage, ServerResponse } from "http";
 import type RouterHandler from "./RouterHandler.js";
-import container from "../dependency-injection/di.js";
 
 export default class Router {
   private readonly routes = new Map<string, RouterHandler>();
-
-  public async init() {
-    const handlers = await container.findByLabel('routeRegistrar');
-    handlers.forEach(handler => this.addRoutes(handler as unknown as RouterHandler));
-  }
-
-
 
   public handle(req:IncomingMessage,res:ServerResponse) {
     const {url} = req
@@ -21,7 +13,6 @@ export default class Router {
     }
 
     const formattedUrl = new URL(url, `http://localhost`)
-    
     const handler = this.routes.get(`${req.method}-${formattedUrl.pathname}`)
     if(!handler){
       res.writeHead(400)
@@ -32,10 +23,12 @@ export default class Router {
     return handler.handle(req, res);
 
   }
-
-  public addRoutes(_:RouterHandler) {
-    const {method, path} = _.data();
-    const key = `${method}-${path}`
-    this.routes.set(key, _)
+  
+  public addRoutes(routeHanlders:RouterHandler[]) {
+    routeHanlders.forEach(_ => {
+      const {method, path} = _.data();
+      const key = `${method}-${path}`
+      this.routes.set(key, _)
+    })
   }
 }
