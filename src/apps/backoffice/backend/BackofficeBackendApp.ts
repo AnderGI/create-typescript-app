85ca603fc96093affb99fb5ec4c94c1f6706ffaa
AnderGI <agibarguen@gmail.com>
2025-06-03 10:48:40 +0200

    refactor: removed server responsibilities from BackofficeBackendApp.
    reasons:
      - this class represents the whole app. it does not need to know if the server running behind is a web server, cli, etc.
      - DIP applied for passing the colaborator through the constructor
    
    BREAKING CHANGE: the public API methods have changed

--- src/apps/backoffice/backend/BackofficeBackendApp.ts
+++ src/apps/backoffice/backend/BackofficeBackendApp.ts
@@ -1,58 +1,14 @@
-import express, { json, Router } from 'express'
-import http from 'http'
-import container from './dependency-injection/diod.config.js';
-import type RouteRegistrar from './routes/RoutesRegistar.js';
+import HttpServer from './HttpServer.js';
 
+export default class BackofficeBackendApp {
 
-export class BackofficeBackendApp {
-	private readonly port: string = '5000';
-	private readonly express: express.Express;
-	private httpServer?: http.Server;
-	private readonly router: express.Router;
-	
-	 constructor() {
-		this.express = express()
-		this.router = Router()
-		this.express.use(json())
-		this.express.use(this.router)
-		this.httpServer = http.createServer(this.express)
-		this.registerRoutes();
-	}
+  constructor(private readonly server: HttpServer){}
 
-	private registerRoutes() {
-		const routeRegistrarIds = container.findTaggedServiceIdentifiers('routeRegistrar');
-		routeRegistrarIds.forEach(id => {
-			const registrar = container.get(id) as RouteRegistrar;
-			registrar.registrar(this.router);
-		});
-	}
+  public start() {
+    this.server.start();
+  }
 
-	async start(): Promise<void> {
-		this.httpServer?.listen(this.port, () => {
-			console.log(`Server listening at http://localhost:${this.port}`)
-		})
-	}
-
-	getServer() {
-			return this.httpServer;
-	}
-
-	async stop(): Promise<void> {
-		return new Promise((resolve, reject) => {
-			if (this.httpServer) {
-				this.httpServer.close(error => {
-					if (error) {
-						reject(error);
-
-						return;
-					}
-
-					resolve();
-				});
-			}
-
-			resolve();
-		});
-	}
-
+  public stop() {
+    this.server.stop();
+  }
 }
