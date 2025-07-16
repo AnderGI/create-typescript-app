568a1ea536b9539c0dbec562c0694c63afc5ef0b
AnderGI <agibarguen@gmail.com>
2025-07-16 17:37:18 +0200

    refactor(status): use http-status constants for response codes

--- src/apps/backoffice/backend/controllers/status/StatusGetController.ts
+++ src/apps/backoffice/backend/controllers/status/StatusGetController.ts
@@ -1,7 +1,8 @@
-import type { Request, Response } from "express";
+import { Request, Response } from "express";
+import status from "http-status";
+
 export default class StatusGetController {
   public run(req:Request, res:Response):void{
-    res.status(200).send();
-    return;
+    res.status(status.OK).send();
   }
 }
