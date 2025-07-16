75dbecb5edcf1de1e06b0e140afe6fa32b1637b3
AnderGI <agibarguen@gmail.com>
2025-07-16 18:23:11 +0200

    refactor(server): relocate RouteHandler and ZaladoAPIGuideBasedRouteValidator to routes directory
    Moves route-related classes from `router-tree/` to `routes/` to better reflect their responsibility and
    reduce ambiguity in the folder structure. Adjusts import paths accordingly in ExpressHttpServer.

--- src/shared/domain/decorator/DomainDecorator.ts
+++
@@ -1,7 +0,0 @@
-const DomainDecorator = (): ClassDecorator => {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
-  return <TFunction extends Function>(target: TFunction): TFunction => {
-    return target
-  }
-}
-export default DomainDecorator
