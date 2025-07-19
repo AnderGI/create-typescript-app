39d5c337273702c06940f4a52fdad5b04928b878
AnderGI <agibarguen@gmail.com>
2025-07-19 17:19:33 +0200

    refactor(route): implement RouteValidator interface in ZaladoAPIGuideBasedRouteValidator

--- src/apps/backoffice/backend/routes/ZaladoAPIGuideBasedRouteValidator.ts
+++ src/apps/backoffice/backend/routes/ZaladoAPIGuideBasedRouteValidator.ts
@@ -1,9 +1,11 @@
+import Route from "./Route";
+import RouteValidator from "./RouteValidator"
 
-
-export default class ZaladoAPIGuideBasedRouteValidator {
+export default class ZaladoAPIGuideBasedRouteValidator implements RouteValidator{
+  // eslint-disable-next-line no-useless-escape
   private readonly ZALANDO_API_GUIDE_BASED_ROUTE_REGEX = new RegExp('^\/(?:(?:[a-z][a-z0-9-]*|\{[A-Za-z0-9_]+\}))(?:\/(?:[a-z][a-z0-9-]*|\{[A-Za-z0-9_]+\}))*$');
 
-  public validatePathRoute(pathName:string) {
+  public validate(route: Route): void {
     // Ensures the route follows Zalando's RESTful API Guidelines.
     //
     // Valid routes must:
@@ -14,14 +16,12 @@ export default class ZaladoAPIGuideBasedRouteValidator {
     //
     // Example: /orders/{orderId}
     // Invalid: /Users, /users//edit, /users/{}
-      if(!this.isValid(pathName)) {
+      if(!this.isValid(route.value)) {
         throw new Error('Invalid Route Pattern. Routes must have the following pattern: /path or /path/{param}, kebab-case segments only.');
-      }
+      }  
   }
-
+  
   public isValid(pathName:string) {
-    return this.ZALANDO_API_GUIDE_BASED_ROUTE_REGEX.test(pathName);
-
+      return this.ZALANDO_API_GUIDE_BASED_ROUTE_REGEX.test(pathName);
   }
-
 }
